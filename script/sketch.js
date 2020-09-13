4e76eafe86718027c79055d2a9fee7475c4d3ad6
510599228@qq.com <510599228@qq.com>
2020-09-13 02:17:21 -0700

    modify models

--- script/sketch.js
+++ script/sketch.js
@@ -225,3 +225,13 @@ function keyPressed() {
     isShowGraphic = 0;
   }
 }
+
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    if (isNaN(time)) {
+      reject(new Error('delay requires a valid number.'));
+    } else {
+      setTimeout(resolve, time);
+    }
+  });
+}
