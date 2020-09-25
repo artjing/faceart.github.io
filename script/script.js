97dedb170766b22c4ab054e4232f398961835d27
510599228@qq.com <510599228@qq.com>
2020-09-25 14:51:37 -0700

    modify video

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const video = document.getElementById('video')
 
 function startVideo() {
-  navigator.mediaDevices.getUserMedia(
+  navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
